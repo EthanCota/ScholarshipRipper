adcfa3cf28994fde07c49f623216f68df99e62c5
EthanCota <46767497+EthanCota@users.noreply.github.com>
2020-02-04 13:38:18 -0800

    Update ScholarshipRipper.py

--- ScholarshipRipper.py
+++ ScholarshipRipper.py
@@ -1,1 +1,12 @@
-import string
+from Selenium import Webdriver
+
+codeList = open('codes.txt','r', encoding='UTF8')
+
+driver = webdriver.Chrome('C:\\chromedriver')
+driver.get('http://ultimatescholarshipbook.com')
+
+for i in range(0, 9999) #TODO Fix 9999 value to correct value
+  code = codeList.readline()
+  
+  driver.find_element_by_name('criteria').sendKeys(code)
+  deriver.find_element_by_tag_name('select').
